148a3853d8feefb24ae6f140d0501acab43cb7d2
磊哥 <493114008@qq.com>
2021-05-10 17:13:42 +0800

    配置 svg plainSprite 报错

---
+++ src/views/Money.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Money.vue
+    </div>
+</template>
+
+<script lang='ts'>
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    import x from '@/assets/icon/money.svg'
+
+    console.log(x);
+    @Component
+    export default class Money extends Vue {
+
+    }
+</script>
+
+<style scoped lang=scss>
+
+</style>
